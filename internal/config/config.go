212c13237b866e67a6777bea3e72f71a4e033e7f
Zhark10 <verygoodman18@yandex.ru>
2022-05-09 01:05:52 +0300

    add configs

---
+++ internal/config/config.go
@@ -0,0 +1,33 @@
+package config
+
+import (
+	"github.com/ilyakaznacheev/cleanenv"
+	"goServer/pkg/logging"
+	"sync"
+)
+
+type Config struct {
+	isDebug *bool `yaml:"is_debug"`
+	Listen  struct {
+		Type   string `yaml:"type"`
+		BindIP string `yaml:"bind_ip"`
+		Port   string `yaml:"port"`
+	} `yaml:"listen"`
+}
+
+var instance *Config
+var once sync.Once
+
+func GetConfig() *Config {
+	once.Do(func() {
+		logger := logging.GetLogger()
+		logger.Info("Read location configuration")
+		instance = &Config{}
+		if err := cleanenv.ReadConfig("config.yml", instance); err != nil {
+			help, _ := cleanenv.GetDescription(instance, nil)
+			logger.Info(help)
+			logger.Fatal(err)
+		}
+	})
+	return instance
+}
