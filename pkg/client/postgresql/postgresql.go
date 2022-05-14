6613fa5a431f8342787204d9b55378b1441a287f
Zhark10 <verygoodman18@yandex.ru>
2022-05-14 09:49:21 +0300

    new postgresql client

--- pkg/client/postgresql/postgresql.go
+++ pkg/client/postgresql/postgresql.go
@@ -2,8 +2,12 @@ package postgresql
 
 import (
 	"context"
+	"fmt"
 	"github.com/jackc/pgconn"
 	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/pgxpool"
+	"log"
+	"time"
 )
 
 type Client interface {
@@ -11,4 +15,48 @@ type Client interface {
 	Query(ctx context.Context, sql string, args ...interface{}) (pgx.Rows, error)
 	QueryRow(ctx context.Context, sql string, args ...interface{}) pgx.Row
 	Begin(ctx context.Context) (pgx.Tx, error)
+}
+
+type StorageConfig struct {
+	Host     string `json:"host"`
+	Port     string `json:"port"`
+	Database string `json:"database"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func NewClient(ctx context.Context, maxAttempts int, sc StorageConfig) (pool *pgxpool.Pool, err error) {
+	dsn := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", sc.Username, sc.Password, sc.Host, sc.Port, sc.Database)
+	err = DoWithTries(func() error {
+		ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+		defer cancel()
+
+		pool, err = pgxpool.Connect(ctx, dsn)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}, maxAttempts, 5*time.Second)
+
+	if err != nil {
+		log.Fatal("error do with tries postgresql")
+	}
+
+	return pool, nil
+}
+
+func DoWithTries(fn func() error, attemtps int, delay time.Duration) (err error) {
+	for attemtps > 0 {
+		if err = fn(); err != nil {
+			time.Sleep(delay)
+			attemtps--
+
+			continue
+		}
+
+		return nil
+	}
+
+	return
 }
