b49638ad30a6b46a4114f569ab9c9954b042a1bc
Zhark10 <verygoodman18@yandex.ru>
2022-05-16 21:55:06 +0300

    fixed func param name

--- pkg/client/postgresql/postgresql.go
+++ pkg/client/postgresql/postgresql.go
@@ -46,11 +46,11 @@ func NewClient(ctx context.Context, maxAttempts int, sc StorageConfig) (pool *pg
 	return pool, nil
 }
 
-func DoWithTries(fn func() error, attemtps int, delay time.Duration) (err error) {
-	for attemtps > 0 {
+func DoWithTries(fn func() error, attempts int, delay time.Duration) (err error) {
+	for attempts > 0 {
 		if err = fn(); err != nil {
 			time.Sleep(delay)
-			attemtps--
+			attempts--
 
 			continue
 		}
